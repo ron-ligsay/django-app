fef6421657680a84e5c70758313fbe5e5d47948d
ron-ligsay <akyligsay16@gmail.com>
2023-08-25 14:06:33 +0800

    importing debug toolbar

--- playground/urls.py
+++ playground/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+import debug_toolbar
+from django.urls import path, include
 from . import views
 
 # URL Configuration (URLConf)
 urlpatterns = [
-    path('hello/',views.say_hello)
+    path('hello/',views.say_hello),
+    path('playground/', include('playground.urls')),
+    path('__debug__/', include(debug_toolbar.urls))
 ]
