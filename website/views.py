6ccfbb0631981b993f206293d854c0364465885f
ron-ligsay <akyligsay16@gmail.com>
2023-08-25 23:22:11 +0800

    login form

--- website/views.py
+++ website/views.py
@@ -3,9 +3,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
+def logout_user(request):
+    pass
 
 # Create your views here.
 def home(request):
+    
     return render(request, 'home.html', {})
 
 
