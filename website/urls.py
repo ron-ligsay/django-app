01f2a257b991c81af19cba64eab9e35fe20b1013
ron-ligsay <akyligsay16@gmail.com>
2023-08-25 22:05:45 +0800

    views"
    git status

--- website/urls.py
+++ website/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ]
