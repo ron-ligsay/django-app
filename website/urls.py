3f41bb666f37d21f91440d34ca05122c00e4028e
ron-ligsay <akyligsay16@gmail.com>
2023-08-25 23:02:43 +0800

    createsuperuser

--- website/urls.py
+++ website/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
 
 # Create your views here.
 def home(request):
