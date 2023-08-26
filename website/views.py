85e151a70158b01013f79df251de8d7bdb9face1
ron-ligsay <akyligsay16@gmail.com>
2023-08-26 13:19:13 +0800

    added models records

--- website/views.py
+++ website/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
+
 
 def logout_user(request):
     logout(request)
@@ -36,6 +38,8 @@ def register_user(request):
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+
     # check to see if logging in
     if request.method == 'POST':
         # attempt to authenticate user
@@ -56,7 +60,7 @@ def home(request):
             messages.success(request, 'Error logging in - please try again...')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 
