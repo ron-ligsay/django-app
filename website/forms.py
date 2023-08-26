ae17ebeb0de71bd7eac96079f98367772b0b7fc5
ron-ligsay <akyligsay16@gmail.com>
2023-08-26 12:24:13 +0800

    forsmt plus url

--- website/forms.py
+++ website/forms.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder': 'Email Address'}),max_length=100, required=True)
+    first_name = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder': 'Password'}),max_length=100, required=True)
+    last_name = forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder': 'Re-Password'}),max_length=100, required=True)
 
 
