49b98711ad05c6ca974b8ed277895c1226e0cf15
ron-ligsay <akyligsay16@gmail.com>
2023-08-26 12:09:13 +0800

    register url

--- website/views.py
+++ website/views.py
@@ -11,6 +11,10 @@ def logout_user(request):
 # def login_user(request):
 #     pass
 
+def register_user(request):
+    return render(request, 'register.html', {})
+
+
 # Create your views here.
 def home(request):
     # check to see if logging in
